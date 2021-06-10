cb1e0befa5635fd5e8c77d0e55a92187a88fb90d
unknown <eugenia.markova1090@gmail.com>
2021-06-10 16:22:29 +0300

    добвлена страница блога

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(function () {
+
+	/* $('.header__btn').on('click', function () {
+		$('.rightside-menu').removeClass('rightside-menu--close');
+	});
+	$('.rightside-menu__close').on('click', function () {
+		$('.rightside-menu').addClass('rightside-menu--close');
+	}); */
+
+	$('.article-slider__box').slick({
+		prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+		/* nextArrow: '<button type="button" class="slick-prev">Previous</button>' */
+	});
+	  
+})
